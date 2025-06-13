31c1a6d9e65f4d30d9fc518f65d6e2f7dcd0be3a
vaibhav30-45 <vshrivastava0730@gmail.com>
2025-06-13 11:24:05 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-
+/*
 
 app.use("/hello",(req, res) => {
     res.end("hello from the server");
@@ -12,6 +12,22 @@ app.use("/hello",(req, res) => {
 app.use((req, res) => {
     res.end("hello from the server");
 });
+*/
+
+app.use(
+    "/user",
+    (req,res,next) => {
+console.log("hello world");
+//res.send ("handler1");
+
+next();
+    }
+,
+
+(req,res) => {
+console.log("hello world");
+res.send ("handler2")   }
+);
 
 
 app.listen(7000,()=>{
