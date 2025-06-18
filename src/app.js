1ac9597dc61a93d768937003c66c0e7860309e76
vaibhav30-45 <vshrivastava0730@gmail.com>
2025-06-18 10:38:07 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,45 +1,79 @@
 
 
-//create a new instance
-
- 
+//create a new instanc
 
 
 const express = require("express");
 const connectDB = require("./config/database");
-const userModel = require("./models/user"); 
-
+const user = require("./models/user");
+const { Console } = require("console");
 
 const app = express();
 
-connectDB();
+// Middleware to parse JSON
+app.use(express.json());
 
+// POST route
 app.post("/signup", async (req, res) => {
-  try {
-    const newUser = new userModel({
-      firstName: "Rohit",
-      lastName: "Sharma",
-      emailId: "rohit3045@gmail.com",
-      password: "rohit@3045",
-      gender: "male",
-      age: 34
-    });
+    const User = new user(req.body);
 
-    await newUser.save();
+  try {
+    
+    await User.save();
     res.send("User added successfully");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error adding user");
+    res.status(400).send("Error adding user");
   }
 });
 
-
-
-
-connectDB();
-
-
-app.listen(7000,()=>{
-    console.log("successfully");
+// GET user by email (using query param)
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    console.log (userEmail);
+    const user = await userModel.findOne({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
- 
+
+// feed API
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Failed to fetch users");
+  }
+});
+
+//update 
+app.patch("/user",async (req,res) => {
+  const userId =req.body.userId;
+  const data = req.body;
+
+  try{
+    const user = await user.findByIdAndUpdate({_id : userId},data,);
+    console.log (user);
+res.send ("user update successfully");
+  }
+  catch (err){
+    res.status(400).send ("something went wrong")
+  }
+});
+  
+
+
+// Connect DB and start server
+connectDB()
+  .then(() => {
+    const PORT = 7000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,16 +1,50 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String },
+  firstName: { type: String,
+    required : "true",
+    minLength :4,
+    maxLength :50,
+  },
   lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  gender: { type: String },
-  age: { type: Number }
-});
+  emailId: { type: String ,
+    required : "true",
+    lowercase : true,
+    unique : true,
+    trim :true,
+
+  },
+  password: { type: String ,
+    required :"true" 
+  },
+  gender: { type: String,
+    validate(value){
+      if (["male","female","others"].includes (value)){
+        throw new Error ("gender data is not valid");
+      }
+
+    }
+   },
+  age: { type: Number },
+  photoUrln: {  
+    type : "String"  },
+     
+     about : {
+      type : "String",
+      default : "this is default value",
+     },
+
+skills : {
+  type :[String],
+},
+},
+{
+  timestamps :true,
+}
+);
 
 //  Yeh line zaroori hai 
-const userModel = mongoose.model("User", userSchema);
+const user = mongoose.model("User", userSchema);
 
 // Export the model
-module.exports = userModel;
+module.exports = user;
